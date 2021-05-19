196936761c76750d9e1aa51db94908a73a0fde9a
Jan Betley <jan.betley@golem.network>
2021-05-19 19:11:48 +0200

    a little more complex test

--- requestor_agent_stub.py
+++ requestor_agent_stub.py
@@ -10,16 +10,31 @@ async def print_status(erigon):
 async def main():
     yem = YagnaErigonManager()
 
+    #   Create 3 erigons
     erigons = []
     for _ in range(0, 3):
-        erigons.append(await yem.deploy_erigon())
+        erigon = await yem.deploy_erigon()
+        await print_status(erigon)
+        erigons.append(erigon)
 
+    #   Check the status concurrently
     tasks = []
     for i in range(3):
         tasks += [print_status(erigon) for erigon in erigons]
-
     await asyncio.gather(*tasks)
 
+    #   Kill one of the erigons
+    stop_result = await erigons[0].stop()
+    erigons = erigons[1:]
+    print(f"Erigon {erigons[0].id} stopped: {stop_result}")
+
+    #   Ensure other erigons are still alive and well
+    tasks = []
+    for i in range(3):
+        tasks += [print_status(erigon) for erigon in erigons]
+    await asyncio.gather(*tasks)
+
+    #   Stop other erigons
     for erigon in erigons:
         stop_result = await erigon.stop()
         print(f"Erigon {erigon.id} stopped: {stop_result}")
