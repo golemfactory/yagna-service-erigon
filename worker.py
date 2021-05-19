8cd0f55278fce583fefeac332f115cb7772d3def
Jan Betley <jan.betley@golem.network>
2021-05-19 17:31:03 +0200

    better worker.py

--- worker.py
+++ worker.py
@@ -1,33 +1,48 @@
 from yapapi import WorkContext
+import json
+
+def parse_result(raw_data):
+    command_executed = raw_data[0]
+    msg = command_executed.message
+    msg = json.loads(msg)
+    stdout = msg['stdout']
+    mock_echo_data, erigon_data = stdout.split('ERIGON: ', 2)
+    erigon_data = json.loads(erigon_data)
+    return erigon_data
+
+
+async def deploy(ctx, erigon):
+    deployment_fut = await erigon.queue.get()
+
+    #   NOTE: this is not necessary, but without any ctx.run() it seems that
+    #   ctx.commit() does nothing and we would deploy only when first request for status
+    ctx.run('STATUS')
+    yield
+    deployment_fut.set_result({'status': 'DEPLOYED'})
+
+
+async def process_commands(ctx, erigon):
+    queue = erigon.queue
+    while True:
+        command, requesting_future = await queue.get()
+        if command == 'STATUS':
+            ctx.run(command)
+            yield requesting_future
+        elif command == 'STOP':
+            requesting_future.set_result({'status': 'STOPPING'})
+            break
 
 
 async def worker(ctx: WorkContext, tasks):
     task = await tasks.__anext__()
     erigon = task.data
-    queue = erigon.queue
 
-    deployment_fut = await queue.get()
+    async for _ in deploy(ctx, erigon):
+        yield ctx.commit()
 
-    print("DEPLOYING")
+    async for requesting_future in process_commands(ctx, erigon):
+        processing_future = yield ctx.commit()
+        result = parse_result(processing_future.result())
+        requesting_future.set_result(result)
 
-    ctx.run('STATUS')
-    yield ctx.commit()
-    deployment_fut.set_result({'status': 'DEPLOYED'})
-    
-    print("AFTER DEPLOYED")
-
-    while True:
-        command, req_fut = await queue.get()
-        if command == 'STATUS':
-            ctx.run(command)
-            out = yield ctx.commit()
-            req_fut.set_result(out.result())
-            queue.task_done()
-        elif command == 'STOP':
-            req_fut.set_result({'status': 'STOPPING'})
-            print("STOPPING")
-            queue.task_done()
-            break
-    
-    print("DONE")
     task.accept_result(result='DONE')
