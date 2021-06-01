d9b3c951c4b9768771779816b4bcd593243ad418
Jan Betley <jan.betley@golem.network>
2021-06-01 18:04:59 +0200

    devnet version bump

--- requestor/run_service_manager.py
+++ requestor/run_service_manager.py
@@ -33,7 +33,7 @@ async def main(sm):
         await asyncio.sleep(1)
 
 if __name__ == '__main__':
-    sm = ServiceManager({'subnet_tag': 'devnet-beta.1'})
+    sm = ServiceManager({'subnet_tag': 'devnet-beta.2'})
     try:
         loop = asyncio.get_event_loop()
         main_task = loop.create_task(main(sm))
