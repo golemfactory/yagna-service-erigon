6f4ebdc80b5dadbfddc8396cfd2205bfbdbf9099
Jan Betley <jan.betley@golem.network>
2021-06-14 13:50:12 +0200

    the same as before, even better

--- requestor/service_manager/service_manager.py
+++ requestor/service_manager/service_manager.py
@@ -6,7 +6,7 @@ from functools import partial
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
-    from typing import Type, List, Any
+    from typing import Type, List, Any, Coroutine
     from yapapi.executor.services import Service
 
 
@@ -24,22 +24,17 @@ async def stop_on_golem_exception(service_manager, e):
     loop.stop()
 
 
-async def restart_on_golem_exception(service_manager, e):
-    pass
-
-
 class ServiceManager():
-    def __init__(self, user_executor_cfg={}, golem_exception_handler='stop'):
-        self.executor_cfg = DEFAULT_EXECUTOR_CFG.copy()
-        self.executor_cfg.update(user_executor_cfg)
-        self.golem_exception_handler = golem_exception_handler
+    def __init__(self, user_executor_cfg={},
+                 golem_exception_handler: 'Coroutine[ServiceManager, Exception]' = stop_on_golem_exception):
+        executor_cfg = DEFAULT_EXECUTOR_CFG.copy()
+        executor_cfg.update(user_executor_cfg)
 
         enable_default_logger(log_file='log.log')
 
-        self._start()
-
-        import os
-        os.environ['YAGNA_APPKEY'] = 'a'
+        self.service_wrappers = []
+        exception_handler = partial(golem_exception_handler, self)
+        self.yapapi_connector = YapapiConnector(executor_cfg, exception_handler)
 
     def create_service(self, service_cls: 'Type[Service]', start_args: 'List[Any]' = []):
         service_wrapper = ServiceWrapper(service_cls, start_args)
@@ -51,24 +46,3 @@ class ServiceManager():
         for service_wrapper in self.service_wrappers:
             service_wrapper.stop()
         await self.yapapi_connector.stop()
-
-    def _start(self):
-        self.service_wrappers = []
-        exception_handler = self._prepare_exception_handler()
-        self.yapapi_connector = YapapiConnector(self.executor_cfg, exception_handler)
-
-    def _prepare_exception_handler(self):
-        handler_func = self._parse_handler_param()
-        return partial(handler_func, self)
-
-    def _parse_handler_param(self):
-        handler_param = self.golem_exception_handler
-
-        if handler_param == 'stop':
-            return stop_on_golem_exception
-        elif handler_param == 'restart':
-            return restart_on_golem_exception
-        elif callable(handler_param):
-            return handler_param
-        else:
-            raise ValueError("golem_exception_handler should be either 'start' or 'restart' or a callable")
