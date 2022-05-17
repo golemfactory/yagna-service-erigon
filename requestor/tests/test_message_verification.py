e1e2bfb4c312195fcaafbca4db4d3cd7eb7a5b14
Rav944 <rav.wozniak94@gmail.com>
2022-05-17 11:14:56 +0200

    fix for pylint

--- requestor/tests/test_message_verification.py
+++ requestor/tests/test_message_verification.py
@@ -8,13 +8,14 @@ class MessageVerificationTestCase(TestCase):
     user_private_key: bytes = b"\xb2\\}\xb3\x1f\xee\xd9\x12''\xbf\t9\xdcv\x9a\x96VK-\xe4\xc4rm\x03[6\xec\xf1\xe5\xb3d"
     message: str = 'First Message'
     message2: str = 'Second Message'
+    account: Account = Account()
 
     def test_correct_validation(self):
         message = encode_defunct(text=self.message)
-        signed_message = Account.sign_message(message, private_key=self.user_private_key)
+        signed_message = self.account.sign_message(signable_message=message, private_key=self.user_private_key)
         self.assertTrue(validate_massage('0x5ce9454909639D2D17A3F753ce7d93fa0b9aB12E', signed_message, self.message))
 
     def test_invalid_validation(self):
         message = encode_defunct(text=self.message2)
-        signed_message = Account.sign_message(message, private_key=self.user_private_key)
+        signed_message = self.account.sign_message(signable_message=message, private_key=self.user_private_key)
         self.assertFalse(validate_massage('0x5ce9454909639D2D17A3F753ce7d93fa0b9aB12E', signed_message, self.message))
